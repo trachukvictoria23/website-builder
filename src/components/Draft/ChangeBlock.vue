b82f7e48f75c1c876005fe171617106984170c47
Victoria Trachuk <victoriatrachuk@voidrp.ru>
2021-02-15 22:39:45 +0200

    Check

--- src/components/Draft/ChangeBlock.vue
+++ src/components/Draft/ChangeBlock.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="change-block">
-    <div class="flex align-center justify-between">
-      <label for="height">
-        {{ $t("height") }}
-      </label>
-      <div>
-        <input
-          name="height"
-          minLength="1"
-          maxLength="3"
-          v-model="height"
-		  class="sidebar__input"
-          @keydown="isNumber"
-          @blur="setNewHeight"
-        />
-        <span>px</span>
-      </div>
-    </div>
-	<button @click="addHeading">
-		Add heading
-	</button>
-  </div>
+	<div class="change-block">
+		<div class="flex align-center justify-between">
+			<label for="height">
+				{{ $t("height") }}
+			</label>
+			<div>
+				<input
+					name="height"
+					minLength="1"
+					maxLength="3"
+					v-model="height"
+					class="sidebar__input"
+					@keydown="isNumber"
+					@blur="setNewHeight"
+				/>
+				<span>px</span>
+			</div>
+		</div>
+		<button @click="addHeading">
+			Add heading
+		</button>
+	</div>
 </template>
 
 <script>
@@ -28,29 +28,29 @@ import { mapGetters } from "vuex";
 import staticMixin from "@/mixins/staticMixin";
 
 export default {
-  data() {
-    return {
-      height: "",
-      heading: "",
-    };
-  },
-  mixins: [staticMixin],
-  computed: {
-    ...mapGetters(["getActiveDraftId"]),
-  },
-  created() {
-    this.height = this.replaceNotDigits(
-      document.getElementById(this.getActiveDraftId).style.height
-    );
-  },
-  methods: {
-    setNewHeight() {
-      this.$store.dispatch("setActiveElementHeight", this.height);
-    },
-    addHeading() {
-      this.$store.dispatch("addNewHeading");
-    },
-  },
+	data() {
+		return {
+			height: "",
+			heading: ""
+		};
+	},
+	mixins: [staticMixin],
+	computed: {
+		...mapGetters(["getActiveDraftId"])
+	},
+	created() {
+		this.height = this.replaceNotDigits(
+			document.getElementById(this.getActiveDraftId).style.height
+		);
+	},
+	methods: {
+		setNewHeight() {
+			this.$store.dispatch("setActiveElementHeight", this.height);
+		},
+		addHeading() {
+			this.$store.dispatch("addNewHeading");
+		}
+	}
 };
 </script>
 
--- src/components/Draft/ChangeText.vue
+++ src/components/Draft/ChangeText.vue
@@ -1,16 +1,16 @@
 <template>
 	<div>
-      <label for="height">
-        {{ $t("heading") }}
-      </label>
-      <div>
-        <textarea
-          name="heading"
-          maxLength="128"
-		  class="sidebar__textarea"
-          v-model="heading"
-          @blur="setNewHeading"
-        />
-      </div>
-    </div>
+		<label for="height">
+			{{ $t("heading") }}
+		</label>
+		<div>
+			<textarea
+				name="heading"
+				maxLength="128"
+				class="sidebar__textarea"
+				v-model="heading"
+				@blur="setNewHeading"
+			/>
+		</div>
+	</div>
 </template>
